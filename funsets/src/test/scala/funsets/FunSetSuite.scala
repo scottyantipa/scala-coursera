a5931abdc48270cc899724efbf678bde5441d910
Scott Antipa <scottantipa@gmail.com>
2016-06-22 21:53:52 -0700

    FunSets

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -77,6 +77,13 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s1_4 = (x: Int) => (x > 0) && (x < 5)
+    val s2_6 = (x: Int) => (x > 1) && (x < 7)
+    val sneg2_2 = (x: Int) => (x > -3) && (x < 3)
+    val all_int = (x: Int) => true
+    val empty = (x: Int) => false
+    val all_pos = (x: Int) => x > 0
+    val all_neg = (x: Int) => x < 0
   }
 
   /**
@@ -110,5 +117,50 @@ class FunSetSuite extends FunSuite {
     }
   }
 
+  test("intersection contains elements that are in each set") {
+    new TestSets {
+      assert(intersect(s1, s2)(3) === false, "Intersection of singletons")
+      assert(intersect(s1_4, s2_6)(4), "Intersection of range")
+    }
+  }
+
+  test("diff contains elements in s that are not in t") {
+    new TestSets {
+      assert(diff(s1, s2)(1), "Diff 1 against s1 and s2")
+      assert(diff(s1_4, s2_6)(1) === true, "Diff range")
+    }
+  }
+
+  test("filter a set" ) {
+    new TestSets {
+      assert( filter(all_int, empty)(1) === false, "False predicate" )
+      assert( filter(sneg2_2, all_pos)(1) === true, "Simple filter" )
+    }
+  }
+
+  test("forall") {
+    new TestSets {
+      assert( forall(s1, s2) === false, "forall of two singletons" )
+      assert( forall(all_pos, (x: Int) => x > -1) === true, "All pos are greater than -1" )
+      assert( forall(all_int, all_pos) === false, "Not all ints are positive" )
+    }
+  }
+
+  test("exists") {
+    new TestSets {
+      assert( exists(s1, s1) === true, "singleton exists in self")
+      assert( exists(sneg2_2, s1) === true, "1 exists in -2 to 2")
+      assert( exists(all_pos, all_neg) === false, "no negatives in all_pos")
+    }
+  }
+
+  test("map") {
+    new TestSets {
+      assert( contains(map(s1, (x: Int) => x + 1), 2) === true, "s1 maps to s2" )
+
+      val posMappedToNeg = map(all_pos, (x: Int) => -x)
+      assert( forall(posMappedToNeg, all_neg) === true, "map pos to neg" )
+    }
+  }
 
 }
